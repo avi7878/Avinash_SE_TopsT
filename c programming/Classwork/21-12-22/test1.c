a37fb704718b97d0e510067c351bde893b701cb5
avi7878 <avinashsinhchauhan7878@gmail.com>
2022-12-21 10:56:46 +0530

    ok

---
+++ c programming/Classwork/21-12-22/test1.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+
+void main()
+{ 
+   printf("FName : avinash");
+   printf("\nLNamde : chauhan");
+   printf("\nAddress : Gir somnath");
+   printf("\nD.O.B : 25 Dec 2001");
+
+}
