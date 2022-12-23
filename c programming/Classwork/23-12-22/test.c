042548ecacfe1c665e66301cb0dd8fe37c5b9856
avi7878 <avinashsinhchauhan7878@gmail.com>
2022-12-23 10:56:14 +0530

    ok

---
+++ c programming/Classwork/23-12-22/test.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+
+void main()
+{
+	//printf("Hello Welcome to C programing");
+	int no = 29;
+	char ch = 'u';
+	float per = 58.78;
+	double stock = 7899987789.45454545;
+	char name [10]="tops tec";
+	char mobile[12]="9594789784";
+    
+    printf("%d\n",no);
+    printf("%c\n",ch);
+    printf("%.2f",per);
+    printf("\n%lf",stock);
+    printf("\n%s",name);
+    printf("%s",mobile);
+
+
+
+
+
+}
