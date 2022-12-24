20126d7c508d73ae69f0b1bdaffa5d074416b080
avi7878 <avinashsinhchauhan7878@gmail.com>
2022-12-24 15:52:00 +0530

    ok

---
+++ c programming/prectice/home.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+void main()
+{
+
+    printf("FName Avinash");
+    printf("\nLName Chauhan");
+    printf("\nAddress Gir somnath");
+    printf("\nD.O.B 25-12-2001");
+    
+
+ 
+   printf("c langunge");
+   printf("\nHello word");
+   printf("\nProgram run");
+   printf("\npython py");   
+
+}
