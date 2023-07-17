96fcf9e293521a496dc583d73f7b9d1861ec94cf
avi7878 <avinashsinhchauhan7878@gmail.com>
2023-07-17 15:41:29 +0530

    Done

---
+++ Python/Assignment/module 3 (Collection,funtion,modules)/54)  Write a Python program to read a random line from a file.py
@@ -0,0 +1,2 @@
+#  Write a Python program to read a random line from a file
+
---
+++ Python/Assignment/module 3 (Collection,funtion,modules)/60)  Write a Python program to find the maximum and minimum numbers  from the specified decimal numbers.py
@@ -0,0 +1,2 @@
+#  Write a Python program to find the maximum and minimum numbers from the specified decimal numbers.
+
